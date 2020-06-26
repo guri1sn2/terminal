ec61d0e0162805e51c46b3e3b934392bec0c76d3
戸塚翔太 <totsukashouta@MacBook-Air.local>
2020-06-26 14:33:44 +0900

    mapのリンク修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'home/show' => 'home#show'
+  get 'home/map' => 'home#map'
   post 'home/master' => "home#master"
   post 'home/update/:id' => "home#register"
   get 'home/:id' => "home#update"
